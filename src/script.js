7a5650c4e2fee7a99cec6b9a829e732d49911960
amycodingmode <giantmotion@gmail.com>
2025-06-14 16:28:33 -0400

    Added the basic search engine (no API)

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function handleSearchSubmit(event) {
+  event.preventDefault(); //default behavior is reloading the page
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
